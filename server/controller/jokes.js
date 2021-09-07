a8063f38ab474595f2b15a129301c4e561a9d284
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-09-07 10:54:41 +0100

    changes

--- server/controller/jokes.js
+++ server/controller/jokes.js
@@ -3,9 +3,27 @@ const router = express.Router();
 
 const Joke = require('../model/joke');
 
+// Retrieving all jokes in JSON format 
+
 router.get('/', (req, res) => {
+
     const jokesData = Joke.all;
     res.send(jokesData);
+})
+
+// Retrieving a joke by Rating
+
+router.get('/:rating', (req, res) => {
+    try {
+       console.log(req.params.rating);
+       const jokeRating = parseInt(req.params.rating);
+       const selectedJoke = Joke.findByRating(jokeRating);
+       res.send(selectedJoke); 
+
+    } catch (error) {
+        console.log(err);
+        res.status(404).send(err);
+    }
 })
 
 module.exports = router;

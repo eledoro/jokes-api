8f201071c031104bbc7ea9f8eaf7d84fad3d3966
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-09-07 10:55:55 +0100

    joke served to client based on rating

--- client/app.js
+++ client/app.js
@@ -2,6 +2,8 @@ let generateJoke = document.getElementById('general-joke-button');
 let jokeContainer = document.getElementById('general-joke-container');
 let jokeUrl = "http://localhost:3000/jokes";
 
+// Generate Random Joke 
+
 generateJoke.addEventListener('click', () => {
     fetch(jokeUrl, {
         method: 'GET',
@@ -18,3 +20,45 @@ generateJoke.addEventListener('click', () => {
             jokeContainer.insertAdjacentHTML("afterend", element);
         })
 })
+
+
+// Click to Receive Joke with your Rating input 
+
+let ratedJoke = document.getElementById('rated-joke');
+let ratedContainer = document.getElementById('rated-joke-container');
+
+ratedJoke.addEventListener('click', () => {
+    // e.preventDefault();
+
+    let inputRating = document.getElementById("rating-input");
+    let rating = inputRating.value;
+    let ratedUrl = `http://localhost:3000/jokes/${rating}`
+
+    console.log(ratedContainer);
+    console.log(inputRating);
+    console.log(rating);
+    if(rating !== ''){
+        fetch(ratedUrl, {
+            method: 'GET',
+        })
+           .then(response => response.json())
+           .then(data = (data) => {
+                console.log(data);
+    
+                // console.log(num);
+                let ratedJokeElement =  `
+                <p>${data.id.text}</p>
+                `;
+                ratedContainer.insertAdjacentHTML("beforeend", ratedJokeElement);
+            })
+    }
+
+})
+
+
+// Clear Jokes container
+let clearButton = document.getElementById('clear-jokes');
+clearButton.addEventListener('click', () => {
+    ratedContainer.textContent = '';
+})
+
